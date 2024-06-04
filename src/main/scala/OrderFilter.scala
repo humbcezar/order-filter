b9af32548e0b53eda9fe7b78276e2aaf6b342ca8
Humberto Cezar <skateforevis@hotmail.com>
2024-06-04 21:56:17 +0100

    6

--- src/main/scala/OrderFilter.scala
+++ src/main/scala/OrderFilter.scala
@@ -5,7 +5,7 @@ case class Order(customerName: String, customerContact: String, shippingAddress:
 case class Item(cost: Double, shippingFee: Double, taxAmount: Double, product: Product)
 case class Product(name: String, category: String, weight: Double, price: Double, date: LocalDate)
 
-class GroupByDateOrderFilter(orders: List[Order], start: LocalDate, end: LocalDate) {
+class GroupByDateOrderFilter(orders: List[Order], start: LocalDate, end: LocalDate, ranges: Seq[String] = Seq()) {
 
   private val startEndDiffInMonths: Long = ChronoUnit.MONTHS.between(start, end)
   private val productOrders: Map[(Long, Long), Seq[(LocalDate, Order)]] = filterAndGroupByDate
@@ -16,31 +16,47 @@ class GroupByDateOrderFilter(orders: List[Order], start: LocalDate, end: LocalDa
       item  <- order.orderItems
     } yield (item.product.date, order)
 
-    productOrders
-      .filterNot(entry => entry._1.isBefore(start))
-      .groupBy(entry => {
-        val monthsAfterStart = ChronoUnit.MONTHS.between(start, entry._1)
-        if (monthsAfterStart > startEndDiffInMonths) (startEndDiffInMonths + 1, startEndDiffInMonths + 1)
-        else if (monthsAfterStart == 0) (0L, 1L)
-        else (monthsAfterStart - 1, monthsAfterStart)
-      })
+    val filteredProducts = productOrders.filterNot(entry => entry._1.isBefore(start))
+    if (ranges.isEmpty) {
+      filteredProducts
+        .groupBy(entry => {
+          val monthsAfterStart = ChronoUnit.MONTHS.between(start, entry._1)
+          if (monthsAfterStart > startEndDiffInMonths) (startEndDiffInMonths + 1, startEndDiffInMonths + 1)
+          else if (monthsAfterStart == 0) (0L, 1L)
+          else (monthsAfterStart - 1, monthsAfterStart)
+        })
+    } else {
+      val parsedRanges: Seq[Array[Int]] = ranges.filterNot(_.contains('>')).map(range => range.split('-').map(_.toInt))
+      val parsedGreaterThanRange: Option[Int] = ranges.find(_.contains('>')).map(range => range.split('>')(1).toInt)
+      filteredProducts
+        .groupBy(entry => {
+          val monthsAfterStart = ChronoUnit.MONTHS.between(start, entry._1)
+          val range = parsedRanges.find(range => monthsAfterStart >= range(0) && monthsAfterStart < range(1)).getOrElse(Array(-1, -1))
+          if (parsedGreaterThanRange.exists(monthsAfterStart > _)) (parsedGreaterThanRange.get, Int.MaxValue)
+          else (range(0), range(1))
+        })
+    }
   }
 
   def prettyPrint(): Unit = {
     productOrders.toList.sortBy(entry => entry._1._1)
+      .filterNot(_._1._1 < 0)
       .foreach(entry => {
-        if (entry._1._1 > startEndDiffInMonths) println(s"> $startEndDiffInMonths months -> " + entry._2.size + " orders")
+        if (entry._1._2 == Int.MaxValue) println(s"> ${entry._1._1} months -> " + entry._2.size + " orders")
+        else if (entry._1._1 > startEndDiffInMonths) println(s"> $startEndDiffInMonths months -> " + entry._2.size + " orders")
         else println(entry._1._1 + "-" + entry._1._2 + " months -> " + entry._2.size + " orders")
       })
-
   }
 }
 
 object OrderFilter {
 
   def main(args: Array[String]): Unit = {
-    val start = LocalDate.parse(args.head)
-    val end = LocalDate.parse(args(1))
+    val (start, end, ranges) = args.toList match {
+      case List(start, end) => (LocalDate.parse(start), LocalDate.parse(end), Seq())
+      case start :: end :: ranges => (LocalDate.parse(start), LocalDate.parse(end), ranges)
+    }
+
     val now = LocalDate.now()
 
     val orders = List(
@@ -54,7 +70,7 @@ object OrderFilter {
       Order("customer", "123", "stree", 10.0, now, List(Item(1.1, 1.1, 1.1, Product("p", "c", 2.2, 2.2, start.plusMonths(16))))),
     )
 
-    val groupByDateOrderFilter = new GroupByDateOrderFilter(orders, start, end)
+    val groupByDateOrderFilter = new GroupByDateOrderFilter(orders, start, end, ranges)
     groupByDateOrderFilter.prettyPrint()
 
   }
